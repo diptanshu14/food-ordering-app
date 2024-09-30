4e200fadd7c3964740e41f53c8b47306c51d784f
diptanshu14 <chattopadhyaydiptanshu@gmail.com>
2024-09-30 12:56:45 +0530

    frontend setup

---
+++ client/src/App.tsx
@@ -0,0 +1,10 @@
+import { Button } from "./components/ui/button"
+import './App.css'
+
+const App = () => {
+  return (
+    <Button className="bg-orange hover:bg-hoverOrange">Food App</Button>
+  )
+}
+
+export default App
