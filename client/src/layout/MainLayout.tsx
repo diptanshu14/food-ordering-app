3b3b57e4181959f59d37ab81dfc32b0918249dde
diptanshu14 <chattopadhyaydiptanshu@gmail.com>
2024-10-02 10:49:45 +0530

    navbar ui

--- client/src/layout/MainLayout.tsx
+++ client/src/layout/MainLayout.tsx
@@ -1,6 +1,12 @@
+import Navbar from "@/components/Navbar"
+
 const MainLayout = () => {
   return (
-    <div>MainLayout</div>
+    <div className="flex flex-col min-h-screen m-2 md:m-0">
+      <header>
+        <Navbar />
+      </header>
+    </div>
   )
 }
 
